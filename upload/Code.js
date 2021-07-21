adeb3f5443e2ab091043f23c11ae7e1ea004b424
gapirajat <86911396+gapirajat@users.noreply.github.com>
2021-07-22 01:32:52 +0530

    Update Code.js

--- upload/Code.js
+++ upload/Code.js
@@ -1,7 +1,6 @@
-function uploadFiles(data)
-{
- var file = data.myFile;
- var folder = DriveApp.getFolderById('1UO8o8rY0cgy2Hz-czB0-w4kUiAHVWUuZ');
- var createFile = folder.createFile(file);
- return createFile.getUrl();
+function uploadFiles(data) {
+   var file = data.myFile;
+   var folder = DriveApp.getFolderById('1UO8o8rY0cgy2Hz-czB0-w4kUiAHVWUuZ');
+   var createFile = folder.createFile(file);
+    return createFile.getUrl();
 }
